579d1dd962355c55b13d75b7a1c79a5f89304dd4
Chris Chenery <Cecer1@cecer1.com>
2011-08-20 07:08:49 +0100

    Initial Commit

---
+++ PluginName.cs
@@ -0,0 +1,15 @@
+﻿namespace IHI.Server.Plugins.AuthorName.PluginName
+{
+    public class PluginName : Plugin
+    {
+        public override void Start()
+        {
+            
+        }
+
+        public override void Stop()
+        {
+            
+        }
+    }
+}
